4b2a8e9b6a96fedc952a9adb7d1881bf7157e62f
EthanSystem <ElementsPhoenix@outlook.com>
2022-06-24 01:27:47 +0800

    Update BaseMachine.h
    构建id；

--- src/Core/Machine/BaseMachine.h
+++ src/Core/Machine/BaseMachine.h
@@ -5,10 +5,19 @@
 #ifndef ENTELECHYSYSTEM_BASEMACHINE_H
 #define ENTELECHYSYSTEM_BASEMACHINE_H
 
+#include <iostream>
 #include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_io.hpp>
+#include <boost/uuid/uuid_generators.hpp>
 
 class BaseMachine {
-
+private:
+    /// 类型名称
+    std::string type_name;
+    /// id
+    boost::uuids::uuid id = boost::uuids::random_generator()();
+    /// id名称
+    std::string uuid_string = boost::uuids::to_string(id);
 };
 
 
