14411dc5105da41fa488cfdfce40300887e49dc4
EthanSystem <ElementsPhoenix@outlook.com>
2022-07-05 15:28:00 +0800

    rename 2022-07-05
    重命名项目之Python包EntelechySystem为PyEntelechySystem；

---
+++ PyEntelechySystem/Core/__init__.py
@@ -0,0 +1,10 @@
+"""
+@File   : __init__.py.py
+@Author : Ethan Lin
+@Date   : 2022/06/19
+@Desc   : 
+"""
+
+from PyEntelechySystem.Core import *
+from PyEntelechySystem.Model import *
+from PyEntelechySystem.World import *
